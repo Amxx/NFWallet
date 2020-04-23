4eb7f4fecd0ced6af074cf5eb31384b8656d977c
Hadrien Croubois <hc@iex.ec>
2020-04-23 19:32:44 +0200

    monorepo

---
+++ solidity/build/flatten/NFWallet.sol
@@ -0,0 +1,233 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+interface IERC721 is IERC165 {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev Returns the number of NFTs in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the NFT specified by `tokenId`.
+     */
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    /**
+     * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
+     * another (`to`).
+     *
+     *
+     *
+     * Requirements:
+     * - `from`, `to` cannot be zero.
+     * - `tokenId` must be owned by `from`.
+     * - If the caller is not `from`, it must be have been allowed to move this
+     * NFT by either {approve} or {setApprovalForAll}.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    /**
+     * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
+     * another (`to`).
+     *
+     * Requirements:
+     * - If the caller is not `from`, it must be approved to move this NFT by
+     * either {approve} or {setApprovalForAll}.
+     */
+    function transferFrom(address from, address to, uint256 tokenId) external;
+    function approve(address to, uint256 tokenId) external;
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    function setApprovalForAll(address operator, bool _approved) external;
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+}
+
+abstract contract IERC721Receiver {
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data)
+    public virtual returns (bytes4);
+}
+
+interface IERC1271
+{
+	function isValidSignature(bytes calldata data, bytes calldata signature) external view returns (bytes4 magicValue);
+}
+
+interface IERC1654
+{
+	function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
+}
+
+contract ECDSA
+{
+	struct signature
+	{
+		uint8   v;
+		bytes32 r;
+		bytes32 s;
+	}
+
+	function recover(bytes32 hash, signature memory sign)
+	internal pure returns (address)
+	{
+		require(sign.v == 27 || sign.v == 28);
+		return ecrecover(hash, sign.v, sign.r, sign.s);
+	}
+
+	function recover(bytes32 hash, bytes memory sign)
+	internal pure returns (address)
+	{
+		bytes32 r;
+		bytes32 s;
+		uint8   v;
+
+		if (sign.length == 65) // 65bytes: (r,s,v) form
+		{
+			assembly
+			{
+				r :=         mload(add(sign, 0x20))
+				s :=         mload(add(sign, 0x40))
+				v := byte(0, mload(add(sign, 0x60)))
+			}
+		}
+		else if (sign.length == 64) // 64bytes: (r,vs) form → see EIP2098
+		{
+			assembly
+			{
+				r :=                mload(add(sign, 0x20))
+				s := and(           mload(add(sign, 0x40)), 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+				v := shr(7, byte(0, mload(add(sign, 0x40))))
+			}
+		}
+		else
+		{
+			revert("invalid-signature-format");
+		}
+
+		if (v < 27) v += 27;
+		require(v == 27 || v == 28);
+		return ecrecover(hash, v, r, s);
+	}
+
+	function toEthSignedMessageHash(bytes32 hash)
+	internal pure returns (bytes32)
+	{
+		return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+	}
+
+	function toEthTypedStructHash(bytes32 struct_hash, bytes32 domain_separator)
+	internal pure returns (bytes32)
+	{
+		return keccak256(abi.encodePacked("\x19\x01", domain_separator, struct_hash));
+	}
+}
+
+contract Context {
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor () internal { }
+
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+abstract contract CounterfactualTokenEntity is Context
+{
+	IERC721 public registry;
+
+	function initialize(address _registry)
+	public virtual
+	{
+		require(address(registry) == address(0), 'already initialized');
+		registry = IERC721(_registry);
+	}
+
+	function owner()
+	public view returns (address)
+	{
+		return registry.ownerOf(uint256(address(this)));
+	}
+
+	modifier onlyOwner()
+	{
+		require(owner() == _msgSender(), 'caller is not the owner');
+		_;
+	}
+}
+
+struct Call
+{
+	address to;
+	uint256 value;
+	bytes   data;
+}
+
+contract NFWallet is CounterfactualTokenEntity, ECDSA, IERC721Receiver, IERC1271, IERC1654
+{
+	event Received(address indexed from, uint256 value);
+
+	// Asset receiving
+	receive()
+	external payable
+	{
+		emit Received(_msgSender(), msg.value);
+	}
+
+	function onERC721Received(address, address, uint256, bytes memory)
+	public override returns (bytes4)
+	{
+		return this.onERC721Received.selector;
+	}
+
+	// Wallet
+	function forward(address to, uint256 value, bytes calldata data)
+	external onlyOwner()
+	{
+		_execute(to, value, data);
+	}
+
+	function forwardBatch(Call[] calldata calls)
+	external onlyOwner()
+	{
+		for (uint256 i = 0; i < calls.length; ++i)
+		{
+			_execute(calls[i].to, calls[i].value, calls[i].data);
+		}
+	}
+
+	function _execute(address to, uint256 value, bytes memory data)
+	internal
+	{
+		(bool success, bytes memory returndata) = payable(to).call{value: value}(data);
+		require(success, string(returndata));
+	}
+
+	function isValidSignature(bytes calldata data, bytes calldata signature)
+	external view override returns (bytes4 magicValue)
+	{
+		require(owner() == recover(keccak256(data), signature));
+		return IERC1271(0).isValidSignature.selector;
+	}
+
+	function isValidSignature(bytes32 hash, bytes calldata signature)
+	external view override returns (bytes4 magicValue)
+	{
+		require(owner() == recover(hash, signature));
+		return IERC1654(0).isValidSignature.selector;
+	}
+}
