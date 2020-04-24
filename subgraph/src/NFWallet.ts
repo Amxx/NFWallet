b5ed6e274f73d3d221049a68fb968cbec9708e3a
Hadrien Croubois <hc@iex.ec>
2020-04-25 00:32:40 +0200

    following subcalls

--- subgraph/src/NFWallet.ts
+++ subgraph/src/NFWallet.ts
@@ -4,9 +4,11 @@ import {
 } from '../generated/templates/NFWallet/NFWallet'
 
 import {
+	Transaction,
 	Account,
 	NFWallet,
 	Received,
+	Executed,
 } from '../generated/schema'
 
 import {
@@ -34,9 +36,20 @@ export function handleReceived(event: ReceivedEvent): void {
 }
 
 export function handleExecuted(event: ExecutedEvent): void {
-	let wallet = NFWallet.load(event.address.toHex());
+	let wallet   = NFWallet.load(event.address.toHex());
+	let executed = new Executed(createEventID(event));
+	let to       = new Account(event.params.to.toHex());
 
 	wallet.balance -= toETH(event.params.value);
 
+	executed.transaction = logTransaction(event).id;
+	executed.timestamp   = event.block.timestamp;
+	executed.wallet      = event.address.toHex();
+	executed.to          = to.id;
+	executed.value       = toETH(event.params.value);
+	executed.data        = event.params.data;
+
 	wallet.save();
+	executed.save();
+	to.save();
 }
