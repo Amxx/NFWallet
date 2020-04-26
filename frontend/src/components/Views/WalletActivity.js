53787bde31e3d328102c9fce69b8314bfd65936c
Hadrien Croubois <hc@iex.ec>
2020-04-26 17:04:27 +0200

    dense activity table

--- frontend/src/components/Views/WalletActivity.js
+++ frontend/src/components/Views/WalletActivity.js
@@ -11,15 +11,15 @@ const WalletActivity = (props) =>
 			{
 				props.data.wallet.events.map(event =>
 					<tr key={event.id}>
-						<td>
-							<strong>{event.__typename}</strong>
+						<td className='py-1 font-weight-bold'>
+							{event.__typename}
 						</td>
-						<td className='text-monospace'>
+						<td className='py-1 text-monospace'>
 							<a className='blue-text' href={`${props.services.config.etherscan}/tx/${event.transaction.id}`} target='_blank' rel='nofollow noopener noreferrer'>
 								{event.transaction.id.slice(0, 10)}...{event.transaction.id.slice(-8)}
 							</a>
 						</td>
-						<td>
+						<td className='py-1 text-right'>
 							<small>{ (new Date(Number(event.timestamp)*1000)).toLocaleString() }</small>
 						</td>
 					</tr>
