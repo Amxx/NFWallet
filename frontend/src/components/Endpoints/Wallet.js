ff1d5a07001b0947a28700f67100fc3fbe66df2d
Hadrien Croubois <hc@iex.ec>
2020-05-04 18:52:36 +0200

    responsive

--- frontend/src/components/Endpoints/Wallet.js
+++ frontend/src/components/Endpoints/Wallet.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import { MDBAlert } from 'mdbreact';
 import { Spinner  } from 'react-bootstrap';
+import { Responsive } from 'responsive-react';
 import { useQuery } from '@apollo/react-hooks';
 import graphql from '../../graphql';
 
 import WithDetails from './Wallet/WithDetails';
 import WalletView  from './Wallet/WalletView';
+import WalletView2 from './Wallet/WalletView2';
+
+const WalletResponsive = (props) =>
+	<>
+		<Responsive displayIn={[ 'Mobile' ]}><WalletView  {...props}/></Responsive>
+		<Responsive displayIn={[ 'Tablet' ]}><WalletView  {...props}/></Responsive>
+		<Responsive displayIn={[ 'Laptop' ]}><WalletView2 {...props}/></Responsive>
+	</>
 
 const Wallet = (props) =>
 {
@@ -23,28 +32,32 @@ const Wallet = (props) =>
 	if (error) { return `Error! ${error}`; }
 
 	return (
-		<div className='container my-auto'>
+		<>
 			{
 				loading &&
-					<div className='d-flex justify-content-center'>
-						<Spinner animation='grow'>
-							<span className='sr-only'>Loading...</span>
-						</Spinner>
+					<div className='container my-auto'>
+						<div className='d-flex justify-content-center'>
+							<Spinner animation='grow'>
+								<span className='sr-only'>Loading...</span>
+							</Spinner>
+						</div>
 					</div>
 			}
 			{
 				data && !data.wallet &&
-					<MDBAlert color='danger' className='text-center font-weight-bold'>
-						No wallet details for { props.routing.match.params.wallet } on { props.services.network.name }
-					</MDBAlert>
+					<div className='container my-auto'>
+						<MDBAlert color='danger' className='text-center font-weight-bold'>
+							No wallet details for { props.routing.match.params.wallet } on { props.services.network.name }
+						</MDBAlert>
+					</div>
 			}
 			{
 				data && data.wallet &&
 					<WithDetails data={data} services={props.services}>
-						<WalletView data={data} services={props.services}/>
+						<WalletResponsive data={data} services={props.services}/>
 					</WithDetails>
 			}
-		</div>
+		</>
 	);
 }
 
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
 import Navbar    from './Navbar';
 import Dashboard from './Endpoints/Dashboard';
 import Overview  from './Endpoints/Overview';
 import Wallet    from './Endpoints/Wallet';
-import Wallet2   from './Endpoints/Wallet2';
-
 
 const Main = (props) =>
 {
@@ -15,8 +14,7 @@ const Main = (props) =>
 			<Route exact path='/'><Redirect to='/dashboard'/></Route>
 			<Route path='/dashboard'            render={ (routing) => <Dashboard routing={routing} {...props}/> }/>
 			<Route path='/overview'             render={ (routing) => <Overview  routing={routing} {...props}/> }/>
-			<Route path='/old-wallet/:wallet?'  render={ (routing) => <Wallet    routing={routing} {...props}/> }/>
-			<Route path='/wallet/:wallet?'      render={ (routing) => <Wallet2   routing={routing} {...props}/> }/>
+			<Route path='/wallet/:wallet?'      render={ (routing) => <Wallet    routing={routing} {...props}/> }/>
 		</Router>
 	);
 }
