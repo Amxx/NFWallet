27bdc89298b5cb6cec106e3b9a5e52e6de9304a6
Hadrien Croubois <hc@iex.ec>
2020-05-06 10:28:52 +0200

    script env

--- subgraph/deploy.sh
+++ subgraph/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/#!/usr/bin/env bash
+#!/bin/bash
 
 graph deploy --debug --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ amxx/non-fungible-wallets-ropsten subgraph.ropsten.yaml &&
 graph deploy --debug --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ amxx/non-fungible-wallets-rinkeby subgraph.rinkeby.yaml &&
